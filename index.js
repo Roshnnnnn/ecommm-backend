1f82e4e716e3c6a58fbc8839a1d6a3b49ee18790
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-09-25 14:29:29 +0530

    product in model completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+	await mongoose.connect("mongodb://127.0.0.1:27017/test");
+	console.log("Database connected");
+}
+
+server.get("/", (req, res) => {
+	res.json({ status: "success" });
+});
+
+server.listen(8080, () => {
+	console.log("server started");
+});
