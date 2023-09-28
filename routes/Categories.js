129203b34df6f64bb22d729412efc282943082ef
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-09-29 00:13:37 +0530

    ..

--- routes/Categories.js
+++ routes/Categories.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { fetchCategories, createCategory } = require("../controller/Category");
 
 const router = express.Router();
-
+//  /categories is already added in base path
 router.get("/", fetchCategories).post("/", createCategory);
 
 exports.router = router;
