b989f0771ee9e6099ce15f930e7b06f20e16fb9f
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-02 02:42:29 +0530

    back

--- index.js
+++ index.js
@@ -22,49 +22,6 @@ const { User } = require("./model/User");
 const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
 
 const SECRET_KEY = "SECRET_KEY";
-
-// Webhook
-
-const endpointSecret =
-	"whsec_ce8473cfb70859aa0b1d30bae200637ddee8fa76140410154c55467c58fd43d6";
-
-server.post(
-	"/webhook",
-	express.raw({ type: "application/json" }),
-	async (request, response) => {
-		const sig = request.headers["stripe-signature"];
-
-		let event;
-
-		try {
-			event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-		} catch (err) {
-			response.status(400).send(`Webhook Error: ${err.message}`);
-			return;
-		}
-
-		// Handle the event
-		switch (event.type) {
-			case "payment_intent.succeeded":
-				const paymentIntentSucceeded = event.data.object;
-				console.log({ paymentIntentSucceeded });
-				const order = await Order.findById(
-					paymentIntentSucceeded.metadata.orderId
-				);
-				order.paymentStatus = "received";
-				await order.save();
-
-				break;
-			// ... handle other event types
-			default:
-				console.log(`Unhandled event type ${event.type}`);
-		}
-
-		// Return a 200 response to acknowledge receipt of the event
-		response.send();
-	}
-);
-
 // JWT options
 
 const opts = {};
@@ -73,7 +30,6 @@ opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 
 //middlewares
 
-server.use(express.raw({ type: "application/json" }));
 server.use(express.static("build"));
 server.use(cookieParser());
 server.use(
@@ -89,6 +45,7 @@ server.use(
 		exposedHeaders: ["X-Total-Count"],
 	})
 );
+server.use(express.raw({ type: "application/json" }));
 server.use(express.json()); // to parse req.body
 server.use("/products", isAuth(), productsRouter.router);
 // we can also use JWT token for client-only auth
@@ -125,7 +82,7 @@ passport.use(
 						return done(null, false, { message: "invalid credentials" });
 					}
 					const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
-					done(null, { token }); // this lines sends to serializer
+					done(null, { id: user.id, role: user.role }); // this lines sends to serializer
 				}
 			);
 		} catch (err) {
@@ -167,6 +124,69 @@ passport.deserializeUser(function (user, cb) {
 		return cb(null, user);
 	});
 });
+
+// Payments
+
+// This is your test secret API key.
+const stripe = require("stripe")(
+	"sk_test_51N5NLVSF2Mo4AGVvozBmb6d5td4kq0lexk43naVyOdmdzLoO4g8LLDsCFc7pT08pUjBesL0G95eP8Xv95kbOaRgU00qrTMpPnY"
+);
+
+server.post("/create-payment-intent", async (req, res) => {
+	const { totalAmount } = req.body;
+
+	// Create a PaymentIntent with the order amount and currency
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: totalAmount * 100, // for decimal compensation
+		currency: "inr",
+		automatic_payment_methods: {
+			enabled: true,
+		},
+	});
+
+	res.send({
+		clientSecret: paymentIntent.client_secret,
+	});
+});
+
+// Webhook
+
+// TODO: we will capture actual order after deploying out server live on public URL
+
+const endpointSecret =
+	"whsec_0e1456a83b60b01b3133d4dbe06afa98f384c2837645c364ee0d5382f6fa3ca2";
+
+server.post(
+	"/webhook",
+	express.raw({ type: "application/json" }),
+	(request, response) => {
+		const sig = request.headers["stripe-signature"];
+
+		let event;
+
+		try {
+			event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+		} catch (err) {
+			response.status(400).send(`Webhook Error: ${err.message}`);
+			return;
+		}
+
+		// Handle the event
+		switch (event.type) {
+			case "payment_intent.succeeded":
+				const paymentIntentSucceeded = event.data.object;
+				console.log({ paymentIntentSucceeded });
+				// Then define and call a function to handle the event payment_intent.succeeded
+				break;
+			// ... handle other event types
+			default:
+				console.log(`Unhandled event type ${event.type}`);
+		}
+
+		// Return a 200 response to acknowledge receipt of the event
+		response.send();
+	}
+);
 
 main().catch((err) => console.log(err));
 
