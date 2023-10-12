7a318f85ce1714d7bad915400fd767769795f279
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-13 00:36:36 +0530

    controller change

--- controller/Product.js
+++ controller/Product.js
@@ -75,3 +75,27 @@ exports.createProduct = async (req, res) => {
 // 		res.status(400).json(err);
 // 	}
 // };
+
+exports.fetchAllProducts = async (req, res) => {
+	const query = Product.find({});
+	if (req.query.category) {
+		query = query.find({ category: req.query.category });
+	}
+	if (req.query.brand) {
+		query = query.find({ brand: req.query.brand });
+	}
+	if (req.query._sort && req.query._order) {
+		query = query.sort({ [req.query._sort]: req.query._order });
+	}
+	if (req.query._page && req.query._limit) {
+		const pageSize = req.query._limit;
+		const page = req.query._page;
+		query = query.skip(pageSize * (page - 1)).limit(pageSize);
+	}
+	try {
+		const doc = await query.exec();
+		res.status(201).json(doc);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+};
--- routes/Products.js
+++ routes/Products.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const {
 	createProduct,
-	// fetchAllProducts,
+	fetchAllProducts,
 	// fetchProductById,
 	// updateProduct,
 } = require("../controller/Product");
 
 const router = express.Router();
 //  /products is already added in base path
-router.post("/", createProduct);
-// .get("/", fetchAllProducts)
+router.post("/", createProduct).get("/", fetchAllProducts);
 // .get("/:id", fetchProductById)
 // .patch("/:id", updateProduct);
 
