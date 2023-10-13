bcd856c67c60f21b9baae3e1a318c0396658ae06
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-13 22:49:17 +0530

    working on sorting

--- controller/Product.js
+++ controller/Product.js
@@ -78,15 +78,27 @@ exports.createProduct = async (req, res) => {
 
 exports.fetchAllProducts = async (req, res) => {
 	let query = Product.find({});
+	let totalProductsQuery = Product.find({});
+
 	if (req.query.category) {
 		query = query.find({ category: req.query.category });
+		totalProductsQuery = totalProductsQuery.find({
+			category: req.query.category,
+		});
 	}
 	if (req.query.brand) {
 		query = query.find({ brand: req.query.brand });
+		totalProductsQuery = totalProductsQuery.find({
+			brand: req.query.brand,
+		});
 	}
 	if (req.query._sort && req.query._order) {
 		query = query.sort({ [req.query._sort]: req.query._order });
 	}
+
+	const totalDocs = await totalProductsQuery.count().exec();
+	console.log({ totalDocs });
+
 	if (req.query._page && req.query._limit) {
 		const pageSize = req.query._limit;
 		const page = req.query._page;
@@ -94,6 +106,7 @@ exports.fetchAllProducts = async (req, res) => {
 	}
 	try {
 		const doc = await query.exec();
+		res.set("X-Total-Count", totalDocs);
 		res.status(201).json(doc);
 	} catch (err) {
 		res.status(400).json(err);
