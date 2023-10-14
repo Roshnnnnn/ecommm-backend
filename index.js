05a6cfb160e318939512b5910176a04159e16709
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-14 14:32:51 +0530

    Auth

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const LocalStrategy = require("passport-local").Strategy;
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
-// const usersRouter = require("./routes/Users");
-// const authRouter = require("./routes/Auth");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
 // const cartRouter = require("./routes/Cart");
 // const ordersRouter = require("./routes/Order");
 // const { User } = require("./model/User");
@@ -41,6 +41,8 @@ server.use(express.json()); // to parse req.body
 server.use("/products", productsRouter.router);
 server.use("/categories", categoriesRouter.router);
 server.use("/brands", brandsRouter.router);
+server.use("/users", usersRouter.router);
+server.use("/auth", authRouter.router);
 // server.use(express.static("build"));
 // server.use(cookieParser());
 // server.use(
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,11 +1,19 @@
+// const express = require("express");
+// const { createUser } = require("../controller/Auth");
+// const passport = require("passport");
+
+// const router = express.Router();
+// //  /auth is already added in base path
+// router.post("/signup", createUser);
+// .post("/login", passport.authenticate("local"), loginUser)
+// .get("/check", passport.authenticate("jwt"), checkAuth);
+// exports.router = router;
+
 const express = require("express");
-const { createUser, loginUser, checkAuth } = require("../controller/Auth");
-const passport = require("passport");
+const { createUser } = require("../controller/Auth");
 
 const router = express.Router();
-//  /auth is already added in base path
-router
-	.post("/signup", createUser)
-	.post("/login", passport.authenticate("local"), loginUser)
-	.get("/check", passport.authenticate("jwt"), checkAuth);
+
+router.post("/", createUser);
+
 exports.router = router;
