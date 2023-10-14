22084c0ac4725b1c4cd5e0ac989e5dacd23ce7e1
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-14 23:34:29 +0530

    login done

--- controller/Auth.js
+++ controller/Auth.js
@@ -9,3 +9,19 @@ exports.createUser = async (req, res) => {
 		res.status(400).json(err);
 	}
 };
+
+exports.loginUser = async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email }).exec();
+		console.log({ user });
+		if (!user) {
+			res.status(401).json({ message: "no such user email" });
+		} else if (user.password === req.body.password) {
+			res.status(200).json({ id: user.id, email: user.email, name: user.name });
+		} else {
+			res.status(401).json({ message: "invalid credentials" });
+		}
+	} catch (err) {
+		res.status(400).json(err);
+	}
+};
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { createUser } = require("../controller/Auth");
+const { createUser, loginUser } = require("../controller/Auth");
 
 const router = express.Router();
 
-router.post("/signup", createUser);
+router.post("/signup", createUser).post("/login", loginUser);
 
 exports.router = router;
