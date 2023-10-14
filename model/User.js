a9931267779df0a6030801e354b0ae849e9b29d9
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-14 22:50:57 +0530

    users.js

--- model/User.js
+++ model/User.js
@@ -5,11 +5,9 @@ const userSchema = new Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	role: { type: String, required: true, default: "user" },
-	// addresses: { type: [Schema.Types.Mixed] },
+	addresses: { type: [Schema.Types.Mixed] },
 	name: { type: String },
-	// orders: { type: [Schema.Types.Mixed] },
-	addresses: { type: [Mixed] },
-	orders: { type: [Mixed] },
+	orders: { type: [Schema.Types.Mixed] },
 });
 
 const virtual = userSchema.virtual("id");
