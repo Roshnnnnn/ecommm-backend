7d0f00558f6b5bf00f5e31ea742a15ed84b3c870
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-16 22:45:02 +0530

    index.js update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const brandsRouter = require("./routes/Brands");
 const usersRouter = require("./routes/Users");
 const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
+const orderRouter = require("./routes/Order");
 
 server.use(
 	cors({
@@ -26,6 +27,7 @@ server.use("/brands", brandsRouter.router);
 server.use("/users", usersRouter.router);
 server.use("/auth", authRouter.router);
 server.use("/cart", cartRouter.router);
+server.use("/orders", orderRouter.router);
 
 server.get("/", (req, res) => {
 	res.json({ status: "success" });
