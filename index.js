29bdc2eb2e60245cd89ad271ffcdefcd03631d07
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-17 15:28:52 +0530

    cart.js updated

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const { createProduct } = require("./controller/Product");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
