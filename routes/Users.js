76654a602941bd7c9f77c6933c389d2dc9ea30bb
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-19 13:18:21 +0530

    backend chnages

--- routes/Users.js
+++ routes/Users.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const { fetchUserById, updateUser } = require("../controller/User");
+const express = require('express');
+const { fetchUserById, updateUser } = require('../controller/User');
 
 const router = express.Router();
 //  /users is already added in base path
-router.get("/:id", fetchUserById).patch("/:id", updateUser);
+router.get('/:id', fetchUserById)
+      .patch('/:id', updateUser)
 
 exports.router = router;
