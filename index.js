a27f019635488bb328a255a181451dcf2a8ccb54
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-20 15:24:56 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
@@ -10,7 +12,46 @@ const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const ordersRouter = require("./routes/Order");
 
+//passport strategies
+
+passport.use(
+	new LocalStrategy(function (username, password, done) {
+		// by default passport uses username
+		User.findOne({ email: username });
+	})
+);
+
+// this creates session variable req.user on being called from callbacks
+
+passport.serializeUser(function (user, cb) {
+	process.nextTick(function () {
+		return cb(null, {
+			id: user.id,
+			username: user.username,
+			picture: user.picture,
+		});
+	});
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+	process.nextTick(function () {
+		return cb(null, user);
+	});
+});
+
 //middlewares
+
+server.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: false,
+		store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+	})
+);
+server.use(passport.authenticate("session"));
 
 server.use(
 	cors({
