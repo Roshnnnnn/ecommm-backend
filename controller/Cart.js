e6e8f89724c13ae1756faa2210089b11507bfdd4
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-24 01:37:58 +0530

    updated

--- controller/Cart.js
+++ controller/Cart.js
@@ -1,9 +1,9 @@
 const { Cart } = require("../model/Cart");
 
 exports.fetchCartByUser = async (req, res) => {
-	const { user } = req.query;
+	const { id } = req.user;
 	try {
-		const cartItems = await Cart.find({ user: user }).populate("product");
+		const cartItems = await Cart.find({ user: id }).populate("product");
 
 		res.status(200).json(cartItems);
 	} catch (err) {
