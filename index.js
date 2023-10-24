4d5e4f25a43d75eb8e467bb667a86edc9350d359
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-25 02:22:00 +0530

    index,js updated

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const cookieParser = require("cookie-parser");
-const LocalStrategy = require("passport-local").Strategy;
+const { createProduct } = require("./controller/Product");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
@@ -19,18 +20,42 @@ const cartRouter = require("./routes/Cart");
 const ordersRouter = require("./routes/Order");
 const { User } = require("./model/User");
 const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
+
 const SECRET_KEY = "SECRET_KEY";
-
-// const token = jwt.sign({ foo: "bar" }, SECRET_KEY);
-
 // JWT options
 
 const opts = {};
 opts.jwtFromRequest = cookieExtractor;
-opts.secretOrKey = SECRET_KEY;
+opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 
-//passport strategies
+//middlewares
 
+server.use(express.static("build"));
+server.use(cookieParser());
+server.use(
+	session({
+		secret: "keyboard cat",
+		resave: false, // don't save session if unmodified
+		saveUninitialized: false, // don't create session until something stored
+	})
+);
+server.use(passport.authenticate("session"));
+server.use(
+	cors({
+		exposedHeaders: ["X-Total-Count"],
+	})
+);
+server.use(express.json()); // to parse req.body
+server.use("/products", isAuth(), productsRouter.router);
+// we can also use JWT token for client-only auth
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/users", isAuth(), usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/orders", isAuth(), ordersRouter.router);
+
+// Passport Strategies
 passport.use(
 	"local",
 	new LocalStrategy({ usernameField: "email" }, async function (
@@ -40,7 +65,8 @@ passport.use(
 	) {
 		// by default passport uses username
 		try {
-			const user = await User.findOne({ email: email }).exec();
+			const user = await User.findOne({ email: email });
+			console.log(email, password, user);
 			if (!user) {
 				return done(null, false, { message: "invalid credentials" }); // for safety
 			}
@@ -50,13 +76,12 @@ passport.use(
 				310000,
 				32,
 				"sha256",
-				async function (err, hashedpassword) {
-					if (!crypto.timingSafeEqual(user.password, hashedpassword)) {
-						done(null, false, { message: "invalid credentials" });
+				async function (err, hashedPassword) {
+					if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+						return done(null, false, { message: "invalid credentials" });
 					}
-
 					const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
-					done(null, { token });
+					done(null, { id: user.id, role: user.role }); // this lines sends to serializer
 				}
 			);
 		} catch (err) {
@@ -69,11 +94,10 @@ passport.use(
 	"jwt",
 	new JwtStrategy(opts, async function (jwt_payload, done) {
 		console.log({ jwt_payload });
-
 		try {
-			const user = await User.findById(jwt_payload);
+			const user = await User.findById(jwt_payload.id);
 			if (user) {
-				return done(null, sanitizeUser(user));
+				return done(null, sanitizeUser(user)); // this calls serializer
 			} else {
 				return done(null, false);
 			}
@@ -84,11 +108,10 @@ passport.use(
 );
 
 // this creates session variable req.user on being called from callbacks
-
 passport.serializeUser(function (user, cb) {
 	console.log("serialize", user);
 	process.nextTick(function () {
-		return cb(null, sanitizeUser(user));
+		return cb(null, { id: user.id, role: user.role });
 	});
 });
 
@@ -96,38 +119,10 @@ passport.serializeUser(function (user, cb) {
 
 passport.deserializeUser(function (user, cb) {
 	console.log("de-serialize", user);
-
 	process.nextTick(function () {
 		return cb(null, user);
 	});
 });
-
-//middlewares
-
-server.use(express.static("build"));
-server.use(cookieParser());
-server.use(
-	session({
-		secret: "keyboard cat",
-		resave: false,
-		saveUninitialized: false,
-	})
-);
-server.use(passport.authenticate("session"));
-
-server.use(
-	cors({
-		exposedHeaders: ["X-Total-Count"],
-	})
-);
-server.use(express.json()); // to parse req.body
-server.use("/products", isAuth(), productsRouter.router);
-server.use("/categories", isAuth(), categoriesRouter.router);
-server.use("/brands", isAuth(), brandsRouter.router);
-server.use("/users", isAuth(), usersRouter.router);
-server.use("/auth", authRouter.router);
-server.use("/cart", isAuth(), cartRouter.router);
-server.use("/orders", isAuth(), ordersRouter.router);
 
 main().catch((err) => console.log(err));
 
