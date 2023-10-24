16dd3553a4c08ce72e07bb4704a7f4995dd3f138
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-25 02:58:28 +0530

    updated

--- model/Order.js
+++ model/Order.js
@@ -1,34 +1,27 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const paymentMethods = {
-	values: ["card", "cash"],
-	message: "enum validator failed for payment Methods",
-};
-const orderSchema = new Schema(
-	{
-		items: { type: [Schema.Types.Mixed], required: true },
-		totalAmount: { type: Number },
-		totalItems: { type: Number },
-		user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-		paymentMethod: { type: String, required: true, enum: paymentMethods },
-		paymentStatus: { type: String, default: "pending" },
-		status: { type: String, default: "pending" },
-		selectedAddress: { type: Schema.Types.Mixed, required: true },
-	},
-	{ timestamps: true }
-);
+const orderSchema = new Schema({
+  items: { type: [Schema.Types.Mixed], required: true },
+  totalAmount: { type: Number },
+  totalItems: { type: Number },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  //TODO:  we can add enum types
+  paymentMethod: { type: String, required: true },
+  status: { type: String, default: 'pending' },
+  selectedAddress: { type: Schema.Types.Mixed, required: true },
+});
 
-const virtual = orderSchema.virtual("id");
+const virtual = orderSchema.virtual('id');
 virtual.get(function () {
-	return this._id;
+  return this._id;
 });
-orderSchema.set("toJSON", {
-	virtuals: true,
-	versionKey: false,
-	transform: function (doc, ret) {
-		delete ret._id;
-	},
+orderSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
 });
 
-exports.Order = mongoose.model("Order", orderSchema);
+exports.Order = mongoose.model('Order', orderSchema);
