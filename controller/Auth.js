6cd1913b06ac757bcca4c4483758660de652a9cb
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-25 02:14:38 +0530

    updated

--- controller/Auth.js
+++ controller/Auth.js
@@ -49,6 +49,10 @@ exports.loginUser = async (req, res) => {
 		.json(req.user.token);
 };
 
-exports.checkUser = async (req, res) => {
-	res.json({ status: "success", user: req.user });
+exports.checkAuth = async (req, res) => {
+	if (req.user) {
+		res.json(req.user);
+	} else {
+		res.sendStatus(401);
+	}
 };
